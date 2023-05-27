14af848d5e2885cc2f8a13394d1c966e9d70341a
Iwan <481427@bathspa.ac.uk>
2023-05-27 12:24:44 +0100

    Added Comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+//variables for each field in the contact page
 const firstName = document.getElementById("fname");
 const surname = document.getElementById("sname");
 const email = document.getElementById("email");
 const message = document.getElementById("subject");
 
+//outputs name, email and message to console
 function submit() {
     console.log(`Name: ${firstName.value} ${surname.value}`);
     console.log(`Email: ${email.value}`);
